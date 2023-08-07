2ceeb468ad37961090dbc94be881f682ffe96027
remboinc <remboinc@mail.ru>
2023-08-07 21:50:15 +0300

    added new logic

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,10 +1,11 @@
 import folium
 import json
 
+from django.forms import model_to_dict
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
-from pokemon_entities.models import Pokemon
+from .models import Pokemon, PokemonEntity
 
 MOSCOW_CENTER = [55.751244, 37.618423]
 DEFAULT_IMAGE_URL = (
@@ -28,17 +29,16 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
-
+    pokemons = PokemonEntity.objects.all()
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon in pokemons:
-        for pokemon_entity in pokemon['entities']:
-            add_pokemon(
-                folium_map, pokemon_entity['lat'],
-                pokemon_entity['lon'],
-                pokemon['img_url']
-            )
+        pokemon_info = model_to_dict(pokemon)
+        add_pokemon(
+            folium_map,
+            pokemon_info['lat'],
+            pokemon_info['lon'],
+            pokemon_info,
+        )
     pokemons = Pokemon.objects.all()
     pokemons_on_page = []
     for pokemon in pokemons:
