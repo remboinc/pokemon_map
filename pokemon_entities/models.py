0fffe9046ec2922515d13411fc3bccda0c2e2369
remboinc <remboinc@mail.ru>
2023-08-14 23:50:49 +0300

    Pokemon descriptions added

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,6 +4,7 @@ from django.db import models  # noqa F401
 class Pokemon(models.Model):
     title = models.TextField(max_length=200)
     image = models.ImageField(upload_to='pokemon_images/', null=True)
+    description = models.TextField(max_length=1000, default="Тут должно быть описание")
 
     def __str__(self):
         return self.title
--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -65,6 +65,7 @@ def show_pokemon(request, pokemon_id):
             'pokemon_id': requested_pokemon.id,
             'img_url': request.build_absolute_uri(requested_pokemon.pokemon.image.url),
             'title_ru': requested_pokemon.pokemon.title,
+            'description': requested_pokemon.pokemon.description
         })
     except PokemonEntity.DoesNotExist:
         return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
