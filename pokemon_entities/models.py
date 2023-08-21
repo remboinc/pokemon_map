dd9c7d99b35b435ee082f8c154f3010830dbece2
remboinc <remboinc@mail.ru>
2023-08-21 20:27:48 +0300

    Added blank=True

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,9 +4,9 @@ from django.db import models  # noqa F401
 class Pokemon(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название покемона')
     image = models.ImageField(upload_to='pokemon_images/', null=True, verbose_name='Изображение покемона')
-    description = models.TextField(verbose_name='Описание', null=True)
-    title_en = models.CharField(max_length=200, null=True, verbose_name='Название на английском')
-    title_jp = models.CharField(max_length=200, null=True, verbose_name='Название на японском')
+    description = models.TextField(verbose_name='Описание', null=True, blank=True)
+    title_en = models.CharField(max_length=200, null=True, blank=True, verbose_name='Название на английском')
+    title_jp = models.CharField(max_length=200, null=True, blank=True, verbose_name='Название на японском')
     evolution = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True,
                                   related_name='related_evolution', verbose_name='Эволюция')
 
