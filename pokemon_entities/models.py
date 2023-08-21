ea95c671492de6f50011b86257de45d7903cc733
remboinc <remboinc@mail.ru>
2023-08-21 20:48:26 +0300

    Rename evolution

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -7,8 +7,7 @@ class Pokemon(models.Model):
     description = models.TextField(verbose_name='Описание', null=True, blank=True)
     title_en = models.CharField(max_length=200, null=True, blank=True, verbose_name='Название на английском')
     title_jp = models.CharField(max_length=200, null=True, blank=True, verbose_name='Название на японском')
-    previous_evolution = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True,
-                                  related_name='related_evolution', verbose_name='Эволюция')
+    previous_evolution = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Предыдущая эволюция')
 
     def __str__(self):
         return self.title
