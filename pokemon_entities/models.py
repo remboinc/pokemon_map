b6b4cdeaad1d601c67a0004c77e01edfbe23f591
remboinc <remboinc@mail.ru>
2023-08-21 20:13:59 +0300

    Added new migration

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,11 +2,11 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    title = models.TextField(max_length=200, verbose_name='Название покемона')
+    title = models.CharField(max_length=200, verbose_name='Название покемона')
     image = models.ImageField(upload_to='pokemon_images/', null=True, verbose_name='Изображение покемона')
     description = models.TextField(max_length=1000, verbose_name='Описание', default="Тут должно быть описание")
-    title_en = models.TextField(max_length=200, default='title', verbose_name='Название на английском')
-    title_jp = models.TextField(max_length=200, default='フシギダネ', verbose_name='Название на японском')
+    title_en = models.CharField(max_length=200, default='title', verbose_name='Название на английском')
+    title_jp = models.CharField(max_length=200, default='フシギダネ', verbose_name='Название на японском')
     evolution = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True,
                                   related_name='related_evolution', verbose_name='Эволюция')
 
