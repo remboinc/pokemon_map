61159ff695ea41d3553e32f96b895489ec964ce4
remboinc <remboinc@mail.ru>
2023-08-23 23:08:31 +0300

    Rename pokemon_entity

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -15,7 +15,7 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, related_name='pokemon_entity', on_delete=models.CASCADE, verbose_name='Что за покемон')
+    pokemon = models.ForeignKey(Pokemon, related_name='entities', on_delete=models.CASCADE, verbose_name='Что за покемон')
     lat = models.FloatField(verbose_name='Широта')
     lon = models.FloatField(verbose_name='Долгота')
     appeared_at = models.DateTimeField(verbose_name='Когда появился')
